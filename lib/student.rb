d4518f9d1c051f4ed4c3cba83e0c6f6f0a4b978e
njmbb8 <nmcgr001@gmail.com>
2021-11-03 00:14:37 -0400

    commit tree a6ef2bcb4de5103af65d198c4cdaa51b81f2b773

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    def initialize(args = {})
+        args.each do |key, value|
+            self.class.attr_accessor(key)
+            self.send("#{key}=", value)
+        end
+    end
 end
